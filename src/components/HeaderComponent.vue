5b3e91a2493e38865b588f0e1c6a80a2306d6f54
FrenzH <franciattom99@gmail.com>
2022-10-08 11:53:30 +0100

    Header Component : NavBarComponent variables.scss

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,50 @@
 <template>
     <header>
-        
+        <div class="header-nav">
+            <div class="logo-div">
+                <img src="@/assets/proj-images/author-logo-round-200x205.png" alt="">
+
+            </div>
+            <NavBarComponent />
+            
+        </div>
     </header>
   
 </template>
 
 <script>
+import NavBarComponent from '@/components/NavBarComponent.vue';
 export default {
     name:'HeaderComponent',
+    components:{
+        NavBarComponent
+    }
 
 }
 </script>
 
 <style lang="scss" scoped>
+header{
+    background-image: url('@/assets/proj-images/author_thumbnail.jpg') ;
+    background-position:contain;
+    background-repeat: no-repeat;
+    background-size: 100%;
+    height: 800px;
+    img{
+        width: 100%;
+    }
+}
+.header-nav{
+    display: flex;
+    justify-content: space-between;
+
+    .logo-div{
+        height: 200px;
+        width:205px;
+        img{
+            width: 100%;
+        }
+    }
+}
 
 </style>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="nav-bar">
+        <ul>
+            <li     
+                   v-for="item in List"  
+                   :key="item">
+                                  {{item}}
+            </li>
+            
+        </ul>
+
+    </div>
+  
+</template>
+
+<script>
+
+export default {
+    name:'NavBarComponent',
+    data(){
+        return{
+           
+            List:['Home','About Me','Testimonials','My Blog','Meetups','Shop','Contact Me']
+        }
+
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss" ;
+li{
+        padding: 10px;
+        color:white;
+        list-style: none;
+    }
+    li:hover{
+        color:$color1;
+        border-top: 2px solid $color1;
+    }
+ul{
+    display: flex;
+    
+    
+    
+}
+
+</style>
